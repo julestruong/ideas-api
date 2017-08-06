a63e904ee4d07be18d2c6908ff03dfeaeb982e33
Jules Truong <jules.truong.perso@gmail.com>
2017-08-06 11:51:08 +0200

    add test

--- security/security.go
+++ security/security.go
@@ -14,15 +14,24 @@ func Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		tokenString := r.Header.Get("Authorization")[7:]
 
-		token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		log.Printf("tokenstring %v", tokenString)
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			log.Printf("token %v", token)
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 			}
-
+			
 			config := app.GetConfig()
 
 			return []byte(config.JWT.Secret), nil
 		})
+		
+		if err != nil {
+			fmt.Println(err)
+
+			return
+		}
 
 		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 			log.Printf("JWT Authenticated OK (app: %s)", claims["app"])
